141b991e039401c5ebc6ff7d3c37fa494536f75b
ALURI SAI LASYA <lasyaaluri@Lasyas-MacBook-Pro.local>
2025-10-31 20:57:34 +0530

    Updated fruit logic and graphics

--- firebase.js
+++ firebase.js
@@ -1,6 +1,4 @@
-// firebase.js
-
-// Import the Firebase modules you need from the CDN
+// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import {
   getAuth,
@@ -16,15 +14,15 @@ import {
   onValue
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-// ✅ Your Firebase Config (from the console)
+// ✅ Firebase Config
 const firebaseConfig = {
-    apiKey: "AIzaSyAKL1LhbQHBJFrjpRMGzklUJdIBSTmV-eM",
-    authDomain: "fruit-ninja-b77dd.firebaseapp.com",
-    projectId: "fruit-ninja-b77dd",
-    storageBucket: "fruit-ninja-b77dd.firebasestorage.app",
-    messagingSenderId: "542497994117",
-    appId: "1:542497994117:web:a4ca8478f280cf60e780ec"
-  };
+  apiKey: "AIzaSyAKL1LhbQHBJFrjpRMGzklUJdIBSTmV-eM",
+  authDomain: "fruit-ninja-b77dd.firebaseapp.com",
+  projectId: "fruit-ninja-b77dd",
+  storageBucket: "fruit-ninja-b77dd.firebasestorage.app",
+  messagingSenderId: "542497994117",
+  appId: "1:542497994117:web:a4ca8478f280cf60e780ec"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -32,7 +30,7 @@ const db = getDatabase(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Export everything you’ll need in game.js
+// Export needed Firebase objects
 export {
   db,
   auth,
